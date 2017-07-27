bbd3d4129b0c7ab4aa045a563fe04c070e970a57
kamimoo <arcobaleno7+git@gmail.com>
2017-07-28 05:33:21 +0900

    Get an access token

--- app/src/test/java/com/github/kamimoo/kotlinarchtecture/api/QiitaServiceTest.kt
+++ app/src/test/java/com/github/kamimoo/kotlinarchtecture/api/QiitaServiceTest.kt
@@ -1,5 +1,6 @@
 package com.github.kamimoo.kotlinarchtecture.api
 
+import com.github.kamimoo.kotlinarchtecture.data.model.TokenParams
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okio.Okio
@@ -47,6 +48,20 @@ class QiitaServiceTest {
         val (title, url) = items.body()!![0]
         assertThat(title).isEqualTo("Android O")
         assertThat(url).isEqualTo("http://qiita.com/a/items/aabbcc123defg04d5ace")
+    }
+
+    @Test
+    fun postAccessTokens() {
+        enqueueResponse("access_tokens.json")
+        val token = service.accessTokens(TokenParams("id", "secret", "code"))
+            .blockingGet()
+        val request = mockWebServer.takeRequest()
+
+        assertThat(request.path).isEqualTo("/access_tokens")
+        assertThat(token).isNotNull()
+        assertThat(token.token)
+            .isEqualTo("ea5d0a593b2655e9568f144fb1826342292f5c6b7d406fda00577b8d1530d8a5");
+        assertThat(token.scopes).containsExactly("read_qiita")
     }
 
     private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
